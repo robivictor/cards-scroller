090a48ff5fca626cc2d9f62bac898caea96d52c3
Robel Herarso <robeltessema.h@gmail.com>
2025-02-05 02:25:48 -0600

    content simulation with some scroll effect

---
+++ components/CardItem.tsx
@@ -0,0 +1,37 @@
+import { LucideIcon } from "lucide-react";
+import Image, { StaticImageData } from "next/image";
+
+interface CardItemProps {
+  i: number;
+  icon: LucideIcon;
+  description: string;
+  image: StaticImageData;
+}
+
+const CardItem = ({ i, icon: Icon, description, image }: CardItemProps) => {
+  return (
+    <div className="h-screen flex items-center justify-center sticky top-0">
+      <div
+        className={`relative bg-[#141a20] rounded-3xl w-[1328px] h-[464px] flex flex-row shadow-2xl shadow-black`}
+        style={{ top: `calc(-5vh + ${-i * 82}px)` }}
+      >
+        <div className="flex flex-col justify-between w-[60%] p-14">
+          <Icon className="h-14 w-14" />
+          <div className="w-2/3">
+             <p className="text-xl text-[#d1d9e1]">{description}</p>
+          </div>
+        </div>
+        <div className="relative w-1/2 h-full rounded-3xl">
+          <Image
+            fill
+            src={image}
+            alt="image"
+            className="object-cover absolute top-0 left-0 w-full h-full rounded-3xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+    return (
+      <section className="w-full flex flex-col items-center justify-center text-center px-4 bg-black text-white mt-[100px]">
+        <div className="max-w-2xl mx-auto">
+          <p className="text-lg sm:text-xl mb-4 font-normal tracking-wide">Turn viewers into paying subscribers</p>
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight">
+            Video monetization platform
+          </h1>
+        </div>
+      </section>
+    )
+  }
